f427e5d0c588a0ee1a6101ee273bf03a3bf27daa
saksham <saksham10.saxena@gmail.com>
2019-03-05 22:02:13 +0530

    Added submit button and message on submitting the Data

--- public/mainController.js
+++ public/mainController.js
@@ -1,6 +1,9 @@
 angular.module('myApp').controller('mainController', function($scope, $http) {
     $scope.data = {};
- 
+    $scope.reset = function() {
+        $scope.user = angular.copy($scope.data);
+        $scope.successMessagebox = false;
+    }
     $scope.submit = function(){
         console.log('clicked submit');
         $http({
@@ -8,6 +11,14 @@ angular.module('myApp').controller('mainController', function($scope, $http) {
             method: 'POST',
             data: $scope.data
         }).then(function (httpResponse) {
+            // $scope.successMessagebox = false;
+            $scope.successMessage = "Data Submitted Successfully"
+            $scope.successMessagebox = true;
+            // $timeout(function() {
+            //     $scope.successMessagebox = false;
+            //     window.location = '/insertingDataUsingAngularjs';
+            // }, 5000);
+            
             console.log('response:', httpResponse);
         })
         // $http.post("/insertingDataUsingAngularjs", data).success(function(data, status) {
